d920a949dbfe3557008424a7837b378907c1fd44
Albert Minnie <Albert.Minnie@imqs.co.za>
2015-11-27 12:53:51 +0200

    Add test that always passes

---
+++ src/test/java/TestStuff.java
@@ -0,0 +1,12 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Created by AlbertM on 2015-11-27.
+ */
+public class TestStuff {
+    @Test
+    public void test_more_stuff() {
+        Assert.assertTrue(true);
+    }
+}
